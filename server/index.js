652c187e9392ba86160c869184291b872018f248
nisarg-gadhiya <163629070+nisarg-gadhiya@users.noreply.github.com>
2026-04-04 13:42:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(cors({
     origin: "https://interview-platform-1-ni5z.onrender.com",
-    "http://interview-platform-1-ni5z.onrender.com"
+    "http://interview-platform-1-ni5z.onrender.com",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
