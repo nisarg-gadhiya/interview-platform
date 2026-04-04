cfb5e2d2789ff02c4bf5769e401a61711c48d835
nisarg-gadhiya <163629070+nisarg-gadhiya@users.noreply.github.com>
2026-04-04 13:45:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: "https://interview-platform-1-ni5z.onrender.com",
-    "http://interview-platform-1-ni5z.onrender.com",
+    origin: ["https://interview-platform-1-ni5z.onrender.com",
+    "http://interview-platform-1-ni5z.onrender.com"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
